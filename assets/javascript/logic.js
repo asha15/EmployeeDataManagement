56209833b276d6b26b7e3266a80aac9b82ba895c
Amanda <amandalragone@gmail.com>
2019-04-13 12:25:48 -0500

    Details are showing on the page!

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,26 +1,31 @@
 $(document).ready(function(){
 
-    var config = {
-        apiKey: "AIzaSyDyFP5cJLmwbxt50gME2tPKBxHdhygNZjw",
-        authDomain: "employeedatamanagement-a98d1.firebaseapp.com",
-        databaseURL: "https://employeedatamanagement-a98d1.firebaseio.com",
-        projectId: "employeedatamanagement-a98d1",
-        storageBucket: "employeedatamanagement-a98d1.appspot.com",
-        messagingSenderId: "284155295511"
-      };
+    // var config = {
+    //     apiKey: "AIzaSyDyFP5cJLmwbxt50gME2tPKBxHdhygNZjw",
+    //     authDomain: "employeedatamanagement-a98d1.firebaseapp.com",
+    //     databaseURL: "https://employeedatamanagement-a98d1.firebaseio.com",
+    //     projectId: "employeedatamanagement-a98d1",
+    //     storageBucket: "employeedatamanagement-a98d1.appspot.com",
+    //     messagingSenderId: "284155295511"
+    //   };
       
-    firebase.initializeApp(config);
+    // firebase.initializeApp(config);
 
-    var database = firebase.database();
+    // var database = firebase.database();
 
     $("button").on("click", function(){
 
         event.preventDefault();
 
-        name = $("#tbl_name").val().trim();
-        role = $("#tbl_role").val().trim();
-        stdate = $("#tbl_stdate").val().trim();
-        monr = parseInt($("#tbl_monr").val().trim());
+        name = $("#frm_name").val();
+        role = $("#frm_role").val();
+        stdate = $("#frm_date").val();
+        monr = parseInt($("#frm_rate").val());
+
+        console.log(name);
+        console.log(role);
+        console.log(stdate);
+        console.log(monr);
 
         var newEmployee = `
             <tr>
